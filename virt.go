839d5ccae0d924bb42d5b1c578fe180e3f19309a
Douile <douile@douile.com>
2024-01-06 01:35:05 +0000

    Add domain state to text responses

--- virt.go
+++ virt.go
@@ -70,11 +70,21 @@ func (p VirtMachine) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.
 		return plugin.NextOrFailure(p.Name(), p.Next, ctx, w, r)
 	}
 
+	answers := []dns.RR{}
+
 	//addrs, err := p.LibVirt.DomainInterfaceAddresses(domPtr, libvirt.DomainInterfaceAddressesSrcLease, 0)
-	ifaces, err := p.LibVirt.DomainInterfaceAddresses(domPtr, 0, 0)
+	ifaces, ifaceErr := p.LibVirt.DomainInterfaceAddresses(domPtr, 0, 0)
 	if err != nil {
 		log.Warningf("Error fetching interface addresses: %v", err)
 		return plugin.NextOrFailure(p.Name(), p.Next, ctx, w, r)
+	}
+
+	var domState int32
+	if state.QType() == dns.TypeTXT {
+		state, _, err := p.LibVirt.DomainGetState(domPtr, 0)
+		if err == nil {
+			domState = state
+		}
 	}
 
 	// TODO: Add disconnect after n secs option
@@ -85,38 +95,66 @@ func (p VirtMachine) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.
 		}
 	}
 
-	answers := []dns.RR{}
-	for _, iface := range ifaces {
-		for _, addr := range iface.Addrs {
-			log.Infof("Replying with address: %s", addr.Addr)
-			ip := net.ParseIP(addr.Addr)
-			if ip.To4() != nil && state.QType() == dns.TypeA {
-				rr := new(dns.A)
-				rr.Hdr = dns.RR_Header{Name: qname, Rrtype: dns.TypeA, Class: dns.ClassINET}
-				rr.A = ip.To4()
+	if ifaceErr == nil {
+		for _, iface := range ifaces {
+			for _, addr := range iface.Addrs {
+				log.Infof("Replying with address: %s", addr.Addr)
+				ip := net.ParseIP(addr.Addr)
+				if ip.To4() != nil && state.QType() == dns.TypeA {
+					rr := new(dns.A)
+					rr.Hdr = dns.RR_Header{Name: qname, Rrtype: dns.TypeA, Class: dns.ClassINET}
+					rr.A = ip.To4()
 
-				answers = append(answers, rr)
-			} else if ip.To4() == nil && ip.To16() != nil && state.QType() == dns.TypeAAAA {
-				rr := new(dns.AAAA)
-				rr.Hdr = dns.RR_Header{Name: qname, Rrtype: dns.TypeAAAA, Class: dns.ClassINET}
-				rr.AAAA = ip.To16()
+					answers = append(answers, rr)
+				} else if ip.To4() == nil && ip.To16() != nil && state.QType() == dns.TypeAAAA {
+					rr := new(dns.AAAA)
+					rr.Hdr = dns.RR_Header{Name: qname, Rrtype: dns.TypeAAAA, Class: dns.ClassINET}
+					rr.AAAA = ip.To16()
 
-				answers = append(answers, rr)
-			} else if state.QType() == dns.TypeTXT {
-				{
-					tt := new(dns.TXT)
-					tt.Hdr = dns.RR_Header{Name: fmt.Sprintf("mask.%s", qname), Rrtype: dns.TypeTXT, Class: dns.ClassINET}
-					tt.Txt = append(tt.Txt, fmt.Sprintf("%s/%d", addr.Addr, addr.Prefix))
-					answers = append(answers, tt)
-				}
-				{
-					tt := new(dns.TXT)
-					tt.Hdr = dns.RR_Header{Name: fmt.Sprintf("if.%s", qname), Rrtype: dns.TypeTXT, Class: dns.ClassINET}
-					tt.Txt = append(tt.Txt, iface.Name)
-					answers = append(answers, tt)
+					answers = append(answers, rr)
+				} else if state.QType() == dns.TypeTXT {
+					{
+						tt := new(dns.TXT)
+						tt.Hdr = dns.RR_Header{Name: fmt.Sprintf("mask.%s", qname), Rrtype: dns.TypeTXT, Class: dns.ClassINET}
+						tt.Txt = append(tt.Txt, fmt.Sprintf("%s/%d", addr.Addr, addr.Prefix))
+						answers = append(answers, tt)
+					}
+					{
+						tt := new(dns.TXT)
+						tt.Hdr = dns.RR_Header{Name: fmt.Sprintf("if.%s", qname), Rrtype: dns.TypeTXT, Class: dns.ClassINET}
+						tt.Txt = append(tt.Txt, iface.Name)
+						answers = append(answers, tt)
+					}
 				}
 			}
 		}
+	}
+
+	if state.QType() == dns.TypeTXT {
+		tt := new(dns.TXT)
+		tt.Hdr = dns.RR_Header{Name: fmt.Sprintf("state.%s", qname), Rrtype: dns.TypeTXT, Class: dns.ClassINET}
+
+		stateName := "unknown"
+		switch libvirt.DomainState(domState) {
+		case libvirt.DomainNostate:
+			stateName = "no-state"
+		case libvirt.DomainBlocked:
+			stateName = "blocked"
+		case libvirt.DomainCrashed:
+			stateName = "crashed"
+		case libvirt.DomainPaused:
+			stateName = "paused"
+		case libvirt.DomainPmsuspended:
+			stateName = "pm-suspended"
+		case libvirt.DomainRunning:
+			stateName = "running"
+		case libvirt.DomainShutdown:
+			stateName = "shutdown"
+		case libvirt.DomainShutoff:
+			stateName = "shutoff"
+		}
+		tt.Txt = append(tt.Txt, stateName)
+		answers = append(answers, tt)
 	}
 
 	m := new(dns.Msg)
